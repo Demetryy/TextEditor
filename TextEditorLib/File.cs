d6f967c711d6e13018a9f73bbd98d85c6f10f6bd
Starinin Andrey <starininandrey@gmail.com>
2021-11-17 21:13:44 +0300

    Инициализация проекта

---
+++ TextEditorLib/File.cs
@@ -0,0 +1,17 @@
+﻿namespace TextEditorLib
+{
+    public abstract class File
+    {
+        protected string _content;
+        protected string _path;
+        protected string _name;
+        protected string _extension;
+
+
+        public abstract void Open();
+        public abstract void Create();
+        public abstract void Close();
+        public abstract void Save();
+        public abstract void SaveAs(string new_path);
+    }
+}
---
+++ TextEditorLib/TxtFile.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace TextEditorLib
+{
+    public class TxtFile : File
+    {
+        public TxtFile() { }
+
+        public TxtFile(string path)
+        {
+            //TODO Проверки на доступность файла или папки
+        }
+        
+        public override void Open()
+        {
+            using var file = new StreamReader(_path);
+            _content = file.ReadToEnd();
+        }
+
+        public override void Create()
+        {
+            
+        }
+
+        public override void Close()
+        {
+            
+        }
+
+        public override void Save()
+        {
+            
+        }
+
+        public override void SaveAs(string new_path)
+        {
+            
+        }
+    }
+}
